65b7397f1d809f77cc3c2914b1c93d1140294a5c
npoojary-techie <60725845+npoojary-techie@users.noreply.github.com>
2020-09-19 17:46:06 +0530

    Add files via upload

---
+++ 3a.py
@@ -0,0 +1,18 @@
+stack = [] 
+stack.append('d') 
+stack.append('a')
+stack.append('m')
+stack.append('n')
+
+
+print('Initial stack') 
+print(stack) 
+
+ 
+print('Elements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+
+print('Stack after elements are poped:') 
+print(stack)
