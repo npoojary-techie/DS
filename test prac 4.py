1620b798cbceb740e61abdfcd1ee258a454cda03
npoojary-techie <60725845+npoojary-techie@users.noreply.github.com>
2020-10-16 15:31:50 +0530

    Add files via upload

---
+++ test prac 4.py
@@ -0,0 +1,20 @@
+
+class Stack():
+    def __init__(self):
+        self.items = [1,2,3,4,5]
+    def enqueing(self,item):
+        self.items.append(item)
+        print(item)  
+        a =self.items
+        print(a)    
+    def dequeing(self):
+        b= self.items
+        b.pop()
+        print(b)
+  
+
+s=Stack()
+print("Adding an element in the queue (enque): ")
+s.enqueing(6)
+print("After removing an element from the queue (deque)): ")
+s.dequeing()
