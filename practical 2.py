7e72b03f52a07a9271e0cf295a39bee47ea191dc
npoojary-techie <60725845+npoojary-techie@users.noreply.github.com>
2020-10-29 11:21:52 +0530

    Update practical 2.py

--- practical 2.py
+++ practical 2.py
@@ -1,38 +1,113 @@
-class Node:    
-    def __init__(self,data):    
-        self.data = data;    
-        self.previous = None;    
-        self.next = None;    
-            
-class DoublyLinkedList:       
-    def __init__(self):    
-        self.head = None;    
-        self.foot = None;      
-    def addNode(self, data):       
-        newNode = Node(data);     
-        if(self.head == None):    
-            self.head = self.foot = newNode;    
-            self.head.previous = None;       
-            self.foot.next = None;    
-        else:    
-            self.foot.next = newNode;    
-            newNode.previous = self.foot;     
-            self.foot = newNode;    
-            self.foot.next = None;    
-    def display(self):    
-        current = self.head;    
-        if(self.head == None):    
-            print("List is empty");    
-            return;    
-        print("Nodes of doubly linked list: ");    
-        while(current != None):       
-            print(current.data),;    
-            current = current.next;    
-                
-dList = DoublyLinkedList();      
-dList.addNode(1);    
-dList.addNode(2);    
-dList.addNode(3);    
-dList.addNode(4);    
-dList.addNode(5);       
-dList.display();  
+class Stack():
+    def __init__(self):
+        self.items = ['4','0','7','6','nishith','abc']
+
+    def end(self, item):
+        self.items.append(item)
+        print(item)
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            return None
+
+    def size(self):
+        if self.items:
+            return len(self.items)
+        else:
+            return None
+
+    def display(self):
+        for i in self.items:
+            print(i)
+
+    def start(self, i):
+        self.items.insert(0, i)
+
+    def search(self, a):
+        l = self.items
+        for i in l:
+            if i == a:
+                print("found Value : ", a)
+                break
+        else:
+            print("not found")
+
+    def traverse(self):
+        a = []
+        l = self.items
+        for i in l:
+            a.append(i)
+        print(a)
+    def shoting_element(self):
+        #bubble shotting
+        nums=self.items
+        def sort(nums):
+            for i in range(len(nums) - 1, 0, -1):
+                for j in range(i):
+                    if nums[j] > nums[j + 1]:
+                        temp = nums[j]
+                        nums[j] = nums[j + 1]
+                        nums[j + 1] = temp
+
+        sort(nums)
+        print(nums)
+    #reverse
+    def reverse(self):
+        l=self.items
+        print(l[::-1])
+
+    def remove_value_from_particular_index(self,a):
+        l=self.items
+        l.pop(a)
+        print(l)
+
+class merge1(Stack):
+    #inheritance
+    def __init__(self):
+        Stack.__init__(self)
+        self.items1 = ['4','3','2','1','6']
+
+    def merge(self):
+        l = self.items
+        l1=self.items1
+        a=(l+l1)
+        a.sort()
+        print(a)
+
+
+
+
+
+
+s = Stack()
+# Inserting the values
+s.end('-1')
+s.start('-2')
+s.start('5')
+s.end('6')
+s.end('7')
+s.start('-1')
+s.start('-2')
+print("search the specific value : ")
+s.search('-2')
+
+print("Display the values one by one :")
+s.display()
+print("peek (End  Value) :", s.peek())
+print("treverse the values : ")
+s.traverse()
+#Shotting element
+print("Shotting the values : ")
+s.shoting_element()
+#reversing the list
+print("Reversing  the values : ")
+s.reverse()
+
+print("remove value from particular index which is defined earlier")
+s.remove_value_from_particular_index(0)
+
+s1=merge1()
+print("merge")
+s1.merge()
